66bcd29cdecab45de7ce7c81aea7c07bee89aab3
Thiago <thiagofreitascarneiro@hotmail.com>
2022-06-17 21:41:32 -0300

    criando controller e inserindo no banco

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Thiago',
+      sobrenome: 'Carneiro',
+      email: 'thiago@gmail.com',
+      idade: 30,
+      peso: 81,
+      altura: 1.85,
     });
+    res.json(novoAluno);
   }
 }
 export default new HomeController();
