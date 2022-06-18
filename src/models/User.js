3e007c5685b54c47b3082205071afa8521cee56b
Thiago <thiagofreitascarneiro@hotmail.com>
2022-06-18 20:45:28 -0300

    criando CRUD de usuarios

---
+++ src/models/User.js
@@ -0,0 +1,58 @@
+import Sequelize, { Model } from 'sequelize';
+import bcryptjs from 'bcryptjs';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'Campo nome deve ter entre 3 e 255 caracteres.',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Email já existe.',
+        },
+        validate: {
+          len: {
+            isEmail: {
+              msg: 'Email invalido.',
+            },
+
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL, // criando o hash baseado na senha do
+        // usuario. Ele não existe no banco de dados.
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 50],
+            msg: 'A senha precisa ter entre 3 a 55 caracteres..',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcryptjs.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+}
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import userController from '../controllers/UserController';
+
+const router = new Router();
+
+router.post('/', userController.store);
+router.get('/', userController.index);
+router.get('/:id', userController.show);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
+
+export default router;
+
+/*
+index -> lista todos os usuários -> GET
+store/create -> cria um novo usuário -> POST
+delete -> apaga um usuário -> DELETE
+show -> mostra um usuário -> GET
+update -> atualiza um usuário -> PATCH ou PUT
+*/
